3da6c7d4fe48a0946d8725acc2e4776593940f20
dharamcr7 <jsracer71@gmail.com>
2019-12-11 23:05:12 +0530

    first

---
+++ app/src/main/java/com/example/rentcar/CarData.java
@@ -0,0 +1,7 @@
+package com.example.rentcar;
+
+class CarData {
+    public String carName =null;
+    public boolean isTrue=false;
+
+}
---
+++ app/src/main/java/com/example/rentcar/CustomerDetail.java
@@ -0,0 +1,15 @@
+package com.example.rentcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_detail);
+
+    }
+}
